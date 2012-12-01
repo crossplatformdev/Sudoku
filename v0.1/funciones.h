a5197e8aef9be0efbf29f49624c6def1301ac77e
Elías A. Angulo Klein <elias.angulo.klein@gmail.com>
2012-12-01 03:39:57 +0100

    Code formatted.

--- v0.1/funciones.h
+++ v0.1/funciones.h
@@ -4,11 +4,11 @@
 #define FILAS 9
 #define COLUMNAS 9
 #define CASILLAS FILAS*COLUMNAS
-#define L_TOTAL L_NOMBRE+L_EXTENSION 
+#define L_TOTAL L_NOMBRE+L_EXTENSION
 #define L_NOMBRE 9
-#define L_EXTENSION 4 
-#define MIN_NUMERO '1' 
-#define MAX_NUMERO '9' 
+#define L_EXTENSION 4
+#define MIN_NUMERO '1'
+#define MAX_NUMERO '9'
 #define PUNTO '.'
 #define MAX_BUFFER 256
 #define MIN_FILA 'A'
@@ -22,19 +22,17 @@
 #define SALIR '3'
 #define MAX_SUBCUADRANTE 3
 
-
-	struct sudoku {
-	char panel[FILAS+1][COLUMNAS+1];	
-	char partida[L_TOTAL+1];
+struct sudoku {
+	char panel[FILAS + 1][COLUMNAS + 1];
+	char partida[L_TOTAL + 1];
 	int espacios;
-	
-	} sk;
 
-	struct coor {
-	char fila;	
+} sk;
+
+struct coor {
+	char fila;
 	char columna;
-	} fc;
-
+} fc;
 
 int Seleccion(); // Muestra el menu de seleccion. Devuelve la seleccion.
 int LeerPartidas(struct sudoku* sk); // Lee las partidas de puzzles.txt.
@@ -46,13 +44,13 @@ void DibujarTablero(struct sudoku* sk); // Dibuja el tablero e imprime informaci
 int LeeTablero(struct sudoku* sk); // Lee el archivo elegido de puzzles.txt.
 int LimpiaTablero(struct sudoku* sk); // Vuelve a inicializar la matrix con el archivo para volverla a su estado original.
 int CompruebaCoordenadas(char *coord); // Comprueba si las coordenadas son correctas
-int CasillaVacia(struct sudoku sk,struct coor fc); // Comprueba si la casilla esta vacia
+int CasillaVacia(struct sudoku sk, struct coor fc); // Comprueba si la casilla esta vacia
 int CompruebaNumero(char * num); // Comprueba si el numero introducido es valido.
 int CambiaNumero(char * num); // Cambia un numero una vez validado que es un numero y no un punto.
-int InsertaNumero(struct sudoku* sk,struct coor fc); //Inserta un numero en el tablero (haciendo las comprobaciones).
+int InsertaNumero(struct sudoku* sk, struct coor fc); //Inserta un numero en el tablero (haciendo las comprobaciones).
 int GuardaTablero(struct sudoku sk); // Guarda el estado del sudoku en un archivo .txt
 int GuardaPartida(char partida[]); // Guarda el nombre del archivo anterior en almacenadas.txt.
 int CargarPartida(struct sudoku *sk); // Inicializa el sudoku a partir de la partida seleccionada de almacenadas.txt.
 int NumeroValido(struct sudoku sk, char coord[], char * num); //Comprueba si un numero es valido.
-int CasillaLlena(struct sudoku sk,struct coor fc); //Comprueba si una casilla esta llena.
-int ModificaNumero(struct sudoku* sk,struct coor fc); // Similar a insertar numero, solo que permite modificar los que ya se han introducido.
+int CasillaLlena(struct sudoku sk, struct coor fc); //Comprueba si una casilla esta llena.
+int ModificaNumero(struct sudoku* sk, struct coor fc); // Similar a insertar numero, solo que permite modificar los que ya se han introducido.
